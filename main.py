02df7e79273e32f5dfa61f31027ab1ef44c50af3
ねんねこ <95109758+nennneko5787@users.noreply.github.com>
2025-10-23 01:00:51 +0900

    main.py を更新

--- main.py
+++ main.py
@@ -4,28 +4,14 @@ import os
 import dotenv
 from discord.ext import commands
 
-from utils.queue import queueLoop
 
 dotenv.load_dotenv()
 
-queueTask: asyncio.Task = None
-
-
-class Bot(commands.Bot):
-    async def close(self):
-        queueTask.cancel()
-        return await super().close()
-
-
-bot = commands.Bot("rin!", help_command=None)
+bot = commands.Bot("rem!", help_command=None)
 
 
 @bot.event
 async def setup_hook():
-    global queueTask
-
-    queueTask = asyncio.create_task(queueLoop())
-
     await bot.load_extension("cogs.aichat")
     await bot.load_extension("cogs.oneday")
     await bot.load_extension("cogs.help")
